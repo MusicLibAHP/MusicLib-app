b148e5bc8c869bd4745e1448a9771f623b42f1bd
DjoAHP <djodjo.vely@gmail.com>
2026-04-04 00:35:40 +0200

    v1.0.1 / Update bouton back ajustement ajout Lucide icon

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hf107m90ja8"
+    "revision": "0.2drt2c7jtdg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
